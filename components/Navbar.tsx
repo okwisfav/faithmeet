96d7e2a7f518ff3cc6b0c770e4772b66b1ff48cb
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2024-11-25 18:06:07 +0100

    .....

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,22 @@
-import { NAV_LINKS } from "@/constants"
-import Image from "next/image"
-import Link from "next/link"
-import Button from "./Button"
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        <Image src="/faithmeeticon" alt="logo" width={74} height={29} />
+        <Image src="/faithmeeticon.svg" alt="logo" width={74} height={29} />
       </Link>
 
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
-          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+          <Link 
+            href={link.href} 
+            key={link.key} 
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
             {link.label}
           </Link>
         ))}
@@ -28,14 +32,14 @@ const Navbar = () => {
       </div>
 
       <Image 
-        src="menu.svg"
+        src="/menu.svg"
         alt="menu"
         width={32}
         height={32}
         className="inline-block cursor-pointer lg:hidden"
       />
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
